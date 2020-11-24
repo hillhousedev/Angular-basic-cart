9a721b4af7cecb165c6a992f360af8ba73534a4d
hillhousedev <adesemoyeolusola@gmail.com>
2020-11-24 22:50:47 +0100

    Product list displaying items and also share button working

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { products } from '../products';
+
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  products = products;
+  constructor() {}
 
-  constructor() { }
+  // tslint:disable-next-line:typedef
+  share() {
+    window.alert('Product is now shared!');
+  }
 
   ngOnInit(): void {
   }
