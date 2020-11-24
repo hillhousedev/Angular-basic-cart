355e615defe671bb0d460f19418e58180a66bb0c
hillhousedev <adesemoyeolusola@gmail.com>
2020-11-24 22:28:27 +0100

    Product data file that consist of an array of objects added

---
+++ src/app/product.ts
@@ -0,0 +1,25 @@
+export const products = [
+    {
+        name: 'Samsung Note 4',
+        price: 500,
+        description: 'A large amoled screen for the best resolution'
+    },
+
+    {
+        name: 'Samsung Galaxy S7',
+        price: 700,
+        description: 'The best durable device'
+    },
+
+    {
+        name: 'Iphone 9',
+        price: 900,
+        decsription: 'Im different from samsung'
+    },
+
+    {
+        name: 'Ipone 12',
+        price: 1500,
+        description: 'Class for boys'
+    }
+];
