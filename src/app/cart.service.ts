9484206cc6c5344ebc3bbcb72647c1b5ed9d4699
hillhousedev <adesemoyeolusola@gmail.com>
2020-11-25 17:57:20 +0100

    Added cart service methods

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { runInThisContext } from 'vm';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+
+  items = [];
+
+  // tslint:disable-next-line:typedef
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  // tslint:disable-next-line:typedef
+  getItems() {
+    return this.items;
+  }
+
+  // tslint:disable-next-line:typedef
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 
   constructor() { }
 }
